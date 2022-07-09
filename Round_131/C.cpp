73e073b8cbee620c06954e3be999cadf330156d1
sbt4104 <sthorat661@gmail.com>
2022-07-10 00:08:40 +0530

    Update C.cpp

--- Round_131/C.cpp
+++ Round_131/C.cpp
@@ -7,47 +7,33 @@ int main() {
     cin>>test;
     while(test--) {
         cin>>N>>M;
-        ans=0;
         vector<ll> vec(N+1,0);
         for(ll index=0; index<M; index++) {
             cin>>val;
             vec[val]++;
         }
-        while(1) {
-            ll count1=0, currMaxi=1000002;
+        // invariant: l is always 0 (false) & r is 1 (true)
+        ll l=0;
+        ll r=10000000000;
+        while(r > l+1) {
+            //cout<<"r: "<<r<<" l: "<<l<<endl;
+            ll mid = (r+l)/2;
+            ll workDone=0;
             for(ll index=1; index<=N; index++) {
-                if(vec[index]!=0) {
-                    currMaxi=min(currMaxi, vec[index]);
-                    count1++;
-                }
-            }
-            //cout<<"currMaxi: "<<currMaxi<<" count1: "<<count1<<endl;
-            if(count1==0) {
-                break;
-            }
-            ans += currMaxi;
-            for(ll index=1; index<=N; index++) {
-                if(vec[index]!=0) {
-                    vec[index]-=currMaxi;
-                }
-            }
-            sort(vec.begin(), vec.end(), greater<ll>());
-            for(ll index=1; index<=N; index++) {
-                if(rest>=vec[index]) {
-                    rest-=vec[index];
-                    vec[index]=0;
+                if(vec[index] >= mid) {
+                    workDone += mid;
                 } else {
-                    rest = 0;
-                    vec[index]-=rest;
+                    workDone += (vec[index] + (mid-vec[index])/2);
                 }
             }
-            if(count1==1) {
-                ll  X = ceil(currMaxi*2/3.0);
-                ans += X;
-                break;
+
+            if(workDone >= M) {
+                r = mid;
+            } else {
+                l = mid;
             }
         }
-        cout<<ans<<endl;
+        cout<<r<<endl;
     }
     return 0;
 }

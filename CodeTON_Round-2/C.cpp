42c103d04fd4c8a1d12170939ccaf491b1426b6e
sbt4104 <sthorat661@gmail.com>
2022-08-01 00:21:14 +0530

    Update C.cpp

--- CodeTON_Round-2/C.cpp
+++ CodeTON_Round-2/C.cpp
@@ -23,13 +23,11 @@ int main() {
             start += n-vec[m-1];
         }
 
-        //cout<<"start: "<<start<<endl;
         if(start>0) {
             interval.push_back(start);
         }
         for(ll index=1; index<m; index++) {
             ll diff = vec[index]-vec[index-1]-1;
-           // cout<<"diff "<<diff<<endl;
             if(diff>0) {
                 interval.push_back(diff);
             }
@@ -37,24 +35,16 @@ int main() {
 
         sort(interval.begin(), interval.end(), greater<ll>());
 
-        // cout<<"intervals\n";
-        // for(ll index=0; index<interval.size(); index++) {
-        //     cout<<interval[index]<<" ";
-        // } cout<<endl;
-
         ll days=0;
         ll save=0;
         for(ll index=0; index<interval.size(); index++) {
             ll total = interval[index]-2*days;
-            //if(vec[index]%2==0 && total==0) {total++;}
-            //cout<<"toal: "<<total<<endl;
             if(total > 0) {
                 if(total==1) {save += total;} else {
                 save += total-1;}
             }
             days+=2;
         }
-
         cout<<n-save<<endl;
     }
     return 0;
